4f419c1a12c54dd7454af7032803acaf9c7d21e4
jorgeassis <jorgemfa@gmail.com>
2021-01-09 22:15:25 +0000

    Up.

--- annotations.py
+++ annotations.py
@@ -4,11 +4,16 @@ import json
 import pandas as pd
 import os
 
+# https://www.robots.ox.ac.uk/~vgg/software/via/
+# https://www.robots.ox.ac.uk/~vgg/software/via/via.html
+
+
 annotationFile = '/Volumes/Jellyfish/GDrive/Manuscripts/Convolutional Neural Networks for kelp canopy identification/Git/maskRCNN/Data/train/via_region_data.json'
 annotations = json.load(open(annotationFile))
 annotations
 
 df = pd.read_json(annotationFile)
 df.head()
+
 
 df.to_json('/Volumes/Jellyfish/GDrive/Manuscripts/Convolutional Neural Networks for kelp canopy identification/Git/maskRCNN/Data/train/via_region_data copy 2.json')
---
+++ dataAccess.py
@@ -0,0 +1,4 @@
+
+# https://pypi.org/project/pylandsat/
+# http://geologyandpython.com/get-landsat-8.html
+# https://towardsdatascience.com/satellite-imagery-access-and-analysis-in-python-jupyter-notebooks-387971ece84b
